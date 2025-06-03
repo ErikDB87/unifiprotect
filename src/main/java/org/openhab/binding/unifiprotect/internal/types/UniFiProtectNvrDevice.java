c2af39143eef03af2941c2a17b06d13322c31340
Erik De Boeck <deboeck.erik@gmail.com>
2025-06-03 13:14:59 +0200

    log uitgebreid naar succesvolle pogingen, nu ook met log van return value
    Signed-off-by: Erik De Boeck <deboeck.erik@gmail.com>

--- src/main/java/org/openhab/binding/unifiprotect/internal/types/UniFiProtectNvrDevice.java
+++ src/main/java/org/openhab/binding/unifiprotect/internal/types/UniFiProtectNvrDevice.java
@@ -374,6 +374,8 @@ public class UniFiProtectNvrDevice {
     public Double getCpuAverageLoad() {
         try {
             logger.debug("getCpuAverageLoad didn't throw an error.");
+            logger.debug("getSystemInfo().getCpu() != null ? getSystemInfo().getCpu().getAverageLoad() : null; = {}",
+                    getSystemInfo().getCpu() != null ? getSystemInfo().getCpu().getAverageLoad() : null);
             return getSystemInfo().getCpu() != null ? getSystemInfo().getCpu().getAverageLoad() : null;
         } catch (Exception getCpuAverageLoadException) {
             logger.debug("getCpuAverageLoadException: {}", getCpuAverageLoadException);
